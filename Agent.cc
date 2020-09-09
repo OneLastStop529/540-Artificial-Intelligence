71582f7c58d5490892ea21ad85a238d0427ed11e
onelaststop529 <yize.hu@wsu.edu>
2020-09-09 13:37:16 -0700

    assignment 2

--- Agent.cc
+++ Agent.cc
@@ -1,5 +1,6 @@
 // Agent.cc
 
+
 #include <iostream>
 #include "Agent.h"
 
@@ -8,6 +9,7 @@ using namespace std;
 Agent::Agent ()
 {
 
+	 
 }
 
 Agent::~Agent ()
@@ -17,38 +19,127 @@ Agent::~Agent ()
 
 void Agent::Initialize ()
 {
-
+	hasGold = false;
+	hasArrow = false;
+	location = std::make_pair(1, 1);
+	orientation = 'r';
 }
 
 Action Agent::Process (Percept& percept)
 {
-	char c;
-	Action action;
-	bool validAction = false;
 
-	while (! validAction)
-	{
-		validAction = true;
-		cout << "Action? ";
-		cin >> c;
-		if (c == 'f') {
-			action = GOFORWARD;
-		} else if (c == 'l') {
-			action = TURNLEFT;
-		} else if (c == 'r') {
-			action = TURNRIGHT;
-		} else if (c == 'g') {
-			action = GRAB;
-		} else if (c == 's') {
-			action = SHOOT;
-		} else if (c == 'c') {
-			action = CLIMB;
-		} else {
-			cout << "Huh?" << endl;
-			validAction = false;
+	Action action;
+
+    // a. If the Glitter percept is True, then execute the GRAB action.
+	// b. If the agent is in the (1,1) location and has the gold, then CLIMB.
+	// c. If the agent has an arrow, and the agent is in the top row (Y=4), and the agent’s
+	// orientation=RIGHT, then SHOOT.
+	// d. If the agent has an arrow, and the agent is in the rightmost column (X=4), and the
+	// agent’s orientation=UP, then SHOOT.
+	// e. If none of the above conditions are met, then the agent should randomly choose one
+	// of the actions: GOFORWARD, TURNLEFT, TURNRIGHT.
+	if (percept.Glitter == true) {
+		action = GRAB;
+		hasGold = true;
+	} else if (hasGold == true && location == std::make_pair(1,1)) {
+		action = CLIMB;
+	} else if (hasArrow == true && orientation == 'r' && location.second == 4){
+		action = SHOOT;
+	} else if (hasArrow == true && orientation == 'u' && location.first == 4) {
+		action = SHOOT;
+	} else {
+		action = (Action) (rand() % 3);	
+		// if agent chooses to go forward update the location coordinates based on 
+		// current location and orientation 
+		if (action == 0) {
+			switch (orientation)
+			{
+			  case 'r':
+				if (location.first != 4)
+					location.first++;
+				break;
+			  case 'l':
+				if (location.first != 1)
+					location.first--;
+				break;
+			  case 'u':
+				if (location.second != 4)
+					location.second++;
+				break;
+			  case 'd':
+				if (location.second != 1)
+					location.second--;
+				break;
+			}
+		}
+		// turning left based on the current orientation
+		if (action == 1) {
+			switch (orientation)
+			{
+			  case 'r':
+				orientation = 'u';
+				break;
+			  case 'l':
+				orientation = 'd';
+				break;
+			  case 'u':
+				orientation = 'l';
+				break;
+			  case 'd':
+				orientation = 'r';
+				break;
+			}
+		}
+		// turing right based on the current orientation
+		if (action == 2)
+		{
+			switch (orientation)
+			{
+			  case 'r':
+				orientation = 'd';
+				break;
+			  case 'l':
+				orientation = 'u';
+				break;
+			  case 'u':
+				orientation = 'r';
+				break;
+			  case 'd':
+				orientation = 'l';
+				break;
+			}
 		}
 	}
+
+
 	return action;
+	// char c;
+	// Action action;
+	// bool validAction = false;
+
+	// while (! validAction)
+	// {
+	// 	validAction = true;
+	// 	cout << "Action? ";
+	// 	cin >> c;
+	// 	if (c == 'f') {
+	// 		action = GOFORWARD;
+	// 	} else if (c == 'l') {
+	// 		action = TURNLEFT;
+	// 	} else if (c == 'r') {
+	// 		action = TURNRIGHT;
+	// 	} else if (c == 'g') {
+	// 		action = GRAB;
+	// 	} else if (c == 's') {
+	// 		action = SHOOT;
+	// 	} else if (c == 'c') {
+	// 		action = CLIMB;
+	// 	} else {
+	// 		cout << "Huh?" << endl;
+	// 		validAction = false;
+	// 	}
+	// }
+	// return action;
 }
 
 void Agent::GameOver (int score)
--- Agent.h
+++ Agent.h
@@ -3,6 +3,7 @@
 #ifndef AGENT_H
 #define AGENT_H
 
+#include <utility>
 #include "Action.h"
 #include "Percept.h"
 
@@ -14,6 +15,11 @@ public:
 	void Initialize ();
 	Action Process (Percept& percept);
 	void GameOver (int score);
+	
+	bool hasGold;
+	bool hasArrow;
+	std::pair<int, int> location;
+	char orientation;
 };
 
 #endif // AGENT_H
